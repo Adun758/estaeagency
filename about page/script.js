63999c34509ed0c90ca13c8732c7af89d20c3aa3
Adun758 <oyesanyaoyinkansola20@gmail.com>
2024-11-26 16:07:32 +0100

    estate Agency

---
+++ about page/script.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector("#menuIcon");
+const navBar = document.querySelector("#navBar");
+const closeBtn = document.querySelector("#closeIcon")
+
+menuBtn.addEventListener("click", menuBtnFunc);
+closeBtn.addEventListener("click", closeIconFunc)
+
+function menuBtnFunc() {
+  navBar.style.transition = " all .8s ease-in-out";
+  navBar.style.top = 0;
+}
+
+function closeIconFunc() {
+    navBar.style.top = "-500px";
+}
